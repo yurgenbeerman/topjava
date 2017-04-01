f5289739811b16b38649c4bd0b4f75228cc1f10c
yurgenbeerman <yurij.pyvovarenko@gmail.com>
2017-04-01 09:16:21 +0300

    Added methods to edit and add meals - and provided concrete - ArrayList - implementation for meals collection.

--- src/main/java/ru/javawebinar/topjava/model/Meals.java
+++ src/main/java/ru/javawebinar/topjava/model/Meals.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.model;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -12,14 +13,14 @@ import java.util.List;
 public class Meals {
     private static int idCounter = 0;
 
-    private static List<Meal> meals = Arrays.asList(
+    private static ArrayList<Meal> meals = new ArrayList<Meal>(Arrays.asList(
             new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500, nextId()),
             new Meal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000, nextId()),
             new Meal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500, nextId()),
             new Meal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000, nextId()),
             new Meal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500, nextId()),
             new Meal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510, nextId())
-    );
+    ));
 
     private static int caloriesPerDay = 2000;
 
@@ -49,6 +50,16 @@ public class Meals {
         return null;
     }
 
+    public static synchronized void  add(LocalDateTime dateTime, String description, int calories) {
+        meals.add(new Meal(dateTime, description, calories, nextId()));
+    }
+
+    public static synchronized void edit(int id, LocalDateTime dateTime, String description, int calories) {
+        Meal meal = getById(id);
+        meal.setDateTime(dateTime);
+        meal.setDescription(description);
+        meal.setCalories(calories);
+    }
 
     private static synchronized int nextId() {
         return idCounter++;
