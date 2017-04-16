b2054a471d8178cab2dd792ac4e461378b38a387
yurgenbeerman <yurij.pyvovarenko@gmail.com>
2017-04-16 17:29:56 +0300

    4 1 HW3

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -11,11 +11,14 @@ import java.time.LocalTime;
 public class Meal extends BaseEntity {
     private Integer id;
 
-    private final LocalDateTime dateTime;
+    private LocalDateTime dateTime;
 
-    private final String description;
+    private String description;
 
-    private final int calories;
+    private int calories;
+
+    public Meal() {
+    }
 
     public Meal(LocalDateTime dateTime, String description, int calories) {
         this(null, dateTime, description, calories);
@@ -50,6 +53,18 @@ public class Meal extends BaseEntity {
 
     public boolean isNew() {
         return id == null;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
     }
 
     @Override
