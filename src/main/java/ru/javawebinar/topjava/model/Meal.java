f90adaf07fbc5a4d32e0192b22804c234898cd86
yurgenbeerman <yurij.pyvovarenko@gmail.com>
2017-04-22 23:13:39 +0300

    5 0 fix refactoring

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -6,10 +6,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
-/**
- * GKislin
- * 11.01.2015.
- */
 public class Meal extends BaseEntity {
     private Integer id;
 
